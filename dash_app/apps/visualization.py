ae9f960d9ee813dc4e907fdef51d686df4cdd62b
Auriane Riou <auriane.riou@gmail.com>
2020-07-22 15:54:27 +0200

    added distributions comparison visualization

--- dash_app/apps/visualization.py
+++ dash_app/apps/visualization.py
@@ -41,9 +41,9 @@ div_graph1 = html.Div(
 
 div_graph2 = html.Div(
     [
-        html.H2("Distribution plots"),
+        html.H2("Scatter plots"),
         dash_table.DataTable(
-            id="df_columns",
+            id="df_columns_scatter",
             column_selectable="multi",
             selected_columns=[],
             virtualization=True,
@@ -72,11 +72,40 @@ div_graph2 = html.Div(
            "flex-direction": "column", "justify-content": "space-evenly", "align-items": "center", "background-color": "#f8f9fa"}
 )
 
+
+div_graph3 = html.Div(
+    [
+        html.H2("Distributions plot"),
+        dash_table.DataTable(
+            id="df_columns_distribution",
+            column_selectable="single",
+            selected_columns=[],
+            virtualization=True,
+            style_table={"overflowX": "auto", "width": 700},
+
+            style_header={"backgroundColor": "rgb(230, 230, 230)", "fontWeight": "bold"}
+        ),
+
+
+        dcc.Graph(
+            id="distr_graph",
+            style={"textAlign": "center"},
+        ),
+
+
+
+    ],
+    style={"marginLeft": 300, "marginTop": 10, "width": "78%", "height": "750px", "display": "flex", "padding": "2rem",
+           "flex-direction": "column", "justify-content": "space-evenly", "align-items": "center", "background-color": "#f8f9fa"}
+)
+
+
+
 layout = html.Div(
     [
         div_graph1,
-        div_graph2
-
+        div_graph2,
+        div_graph3
     ],
     style={"display": "flex", "flex-direction": "column"},
     id="results_layout"
@@ -104,19 +133,20 @@ def store_initial_pearson_plot(data):
 
 
 @app.callback(
-    Output("df_columns", "columns"),
+    [Output("df_columns_scatter", "columns"),
+     Output("df_columns_distribution", "columns")],
     [Input("storage_sample_df", "data")]
 )
 def update_initial_table(jsonified_data):
     df_gen = pd.read_json(jsonified_data, orient="split")
     col_gen = [{"name": i, "id": i, "selectable": True} for i in df_gen.columns],
-    return col_gen[0]
+    return col_gen[0], col_gen[0]
 
 
 @app.callback(
     [Output("plot_graph_init", "figure"),
      Output("plot_graph_gen", "figure")],
-    [Input("df_columns", "selected_columns"),
+    [Input("df_columns_scatter", "selected_columns"),
      Input("storage_generated_table_num", "data"),
      Input("storage_sample_df_num", "data"),
      Input("storage_types", "data"),]
@@ -142,6 +172,32 @@ def renders_plot(selected_columns, jsonified_data1, jsonified_data2, types):
 
         return fig_init, fig_gen
 
+
+@app.callback(
+    Output("distr_graph", "figure"),
+    [Input("df_columns_distribution", "selected_columns"),
+     Input("storage_generated_table_num", "data"),
+     Input("storage_sample_df_num", "data"),
+     Input("storage_types", "data"),]
+)
+def renders_plot(selected_columns, jsonified_data1, jsonified_data2, types):
+    df_gen_num = pd.read_json(jsonified_data1, orient="split")
+    df_sample_num = pd.read_json(jsonified_data2, orient="split")
+
+    if types[selected_columns[0]] == "Categorical":
+        attribute = selected_columns[0] + "_NUM"
+    else:
+        attribute = selected_columns[0]
+
+    fig = go.Figure()
+    fig.add_trace(go.Histogram(x=df_gen_num[attribute], name="Generated dataframe") )
+    fig.add_trace(go.Histogram(x=df_sample_num[attribute], name="Initial dataframe"))
+
+    fig.update_layout(barmode='overlay')
+
+    fig.update_traces(opacity=0.75)
+
+    return fig
 
 
 

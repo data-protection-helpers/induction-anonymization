37c7f15a801a1e1eeadbfdffa98a247cd1560df2
Auriane Riou <auriane.riou@gmail.com>
2020-07-28 15:53:20 +0200

    resolved change in type issue for masked attributes

--- dash_app/masking.py
+++ dash_app/masking.py
@@ -1,11 +1,13 @@
 def complete_masking(df, attributes):
+    #print("df", df)
     df_masked = df.copy()
-    for index, row in df.iterrows():
+    #print("df masked", df_masked)
+    for row in df_masked.itertuples():
         for attribute in attributes:
             masked_val = []
-            val = str(row[attribute])
+            val = str( getattr(row, attribute))
             for i in range(len(val)):
                 masked_val.append("#")
             processed = "".join(masked_val)
-            df_masked.loc[index, attribute] = processed
+            df_masked.loc[row.Index, attribute] = processed
     return df_masked
